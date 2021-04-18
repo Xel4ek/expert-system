8cba385122d7e1b2e4e9d3ee8cd9358429f4d7ec
Xel4ek <l-baal@yandex.ru>
2021-04-19 00:01:53 +0300

    drag and drop

--- src/app/components/expert/welcome/welcome.component.ts
+++ src/app/components/expert/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
+import { first, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-welcome',
@@ -6,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
+  test$: Observable<string>[] = [];
   constructor() {}
 
   ngOnInit(): void {}
+  uploadFile(files: FileList): void {
+    Array.from(files).map((file) => {
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      const data$ = fromEvent(reader, 'load').pipe(
+        first(),
+        map((result) => {
+          let binary = '';
+          const bytes = new Uint8Array((result?.target as any).result);
+          const length = bytes.byteLength;
+          for (let i = 0; i < length; i++) {
+            binary += String.fromCharCode(bytes[i]);
+          }
+          return binary;
+        })
+      );
+      this.test$.push(data$);
+    });
+  }
 }
